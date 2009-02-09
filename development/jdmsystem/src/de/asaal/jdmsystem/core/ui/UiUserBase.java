41c3fb670295d2b800f5ad2fe958f47be181fb58
chmaster <chmaster@ff0c6f71-da43-0410-abee-ea3d5be7fece>
2009-02-09 22:09:46 +0000

    removed unused qrc

--- development/jdmsystem/src/de/asaal/jdmsystem/core/ui/UiUserBase.java
+++ development/jdmsystem/src/de/asaal/jdmsystem/core/ui/UiUserBase.java
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'UserBase.jui'
 **
-** Created: Fr 6. Feb 10:38:29 2009
+** Created: Mo 9. Feb 23:09:09 2009
 **      by: Qt User Interface Compiler version 4.4.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
